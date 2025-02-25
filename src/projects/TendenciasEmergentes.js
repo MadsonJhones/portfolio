4510680432b453f4abfd14fca404af1608c5fd9c
MadsonJhones <madsoncell@gmail.com>
2025-02-25 16:28:49 -0300

    Carteira Web3

--- src/projects/TendenciasEmergentes.js
+++ src/projects/TendenciasEmergentes.js
@@ -1,18 +1,29 @@
 // src/projects/TendenciasEmergentes.js
 import { getNextId } from "./idCounter.js";
-import defaultImage from "../assets/images/default-image.png";
+import carteiraimage1 from "../assets/images/tendencias/carteira-web3/carteira-image1.png";
+import carteiraimage2 from "../assets/images/tendencias/carteira-web3/carteira-image2.png";
+import carteiraimage3 from "../assets/images/tendencias/carteira-web3/carteira-image3.png";
+import carteiraimage4 from "../assets/images/tendencias/carteira-web3/carteira-image4.png";
 
 const TendenciasEmergentesProjects = [
     {
         id: getNextId(), // Gera automaticamente um ID único
-        title: "Web3 DApp",
+        title: "Carteira Web3",
         category: "Tendências Emergentes",
-        images: [defaultImage], // Apenas uma imagem principal
-        popupImages: [defaultImage],
-        description: "Aplicação descentralizada (DApp) baseada em Web3, utilizando Solidity e React para interações com blockchain.",
-        tags: ["React", "Solidity", "Web3.js"],
+        images: [carteiraimage1], // Apenas uma imagem principal
+        popupImages: [
+            carteiraimage1,
+            carteiraimage2,
+            carteiraimage3,
+            carteiraimage4,
+        ],
+        description: "A Carteira Web3 é um mockup em React com TypeScript, simulando gestão de criptoativos com interface moderna, segura e inspirada na Binance, essencial para protótipos Web3.",
+        tags: ["React", "Typescript"],
     },
-    // Continue adicionando mais projetos (até 10), usando getNextId() para cada novo projeto
+    // Continue adicionando mais projetos, usando getNextId() para cada novo projeto
 ];
 
 export default TendenciasEmergentesProjects;
+
+
+
